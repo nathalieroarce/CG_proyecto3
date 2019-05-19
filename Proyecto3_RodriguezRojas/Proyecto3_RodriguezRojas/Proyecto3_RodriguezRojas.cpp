5664e25e0e3306c76d85ece24dec3216774eeb75
Vinicio <viniciorodriguez97@gmail.com>
2019-05-19 00:08:18 -0600

    Cambios main

--- Proyecto3_RodriguezRojas/Proyecto3_RodriguezRojas/Proyecto3_RodriguezRojas.cpp
+++ Proyecto3_RodriguezRojas/Proyecto3_RodriguezRojas/Proyecto3_RodriguezRojas.cpp
@@ -8,98 +8,196 @@
 #include <istream>
 
 
-unsigned char* readBMP(const char* filename)
-{
-	int i;
-	FILE* f;
-	fopen_s(&f, filename, "rb");
-	unsigned char info[54];
-	fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
-
-	// extract image height and width from header
-	int width = *(int*)&info[18];
-	int height = *(int*)&info[22];
-	int size = 3 * width * height;
-
-	unsigned char* data = new unsigned char[size]; // allocate 3 bytes per pixel
-	fread(data, sizeof(unsigned char), size, f); // read the rest of the data at once
-	fclose(f);
-
-	for (i = 0; i < size; i += 3)
-	{
-		unsigned char tmp = data[i] / 255;
-		data[i] = data[i + 2] / 255;
-		data[i + 2] = tmp;
-	}
-
-	return data;
-}
-
 void createScene()
 {
 	using namespace Imager;
 
 	Scene scene(Color(0.0, 0.0, 0.0));
 
-	printf("read image \n");
-	unsigned char* bg = readBMP("backGround.bmp");
+	//rintf("read image \n");
 
-	const double innerSize = 10.00;
-	const double xBorderSize = 1.00;
-	const double yBorderSize = 1.00;
-	const double thickness = 0.20;
+		//unsigned char* bg = readBMP("backGround.bmp");
 
-	const Color lightSquareColor = Color(0.40, 0.30, 0.5);
-	const Color darkSquareColor = Color(0.40, 0.30, 0.5);
-	const Color borderColor = Color(0.40, 0.30, 0.5);
+		/*const double innerSize = 10.00;
+		const double xBorderSize = 1.00;
+		const double yBorderSize = 1.00;
+		const double thickness = 0.20;
 
-	// Create the chess board and add it to the scene.
+		const Color lightSquareColor = Color(0.40, 0.30, 0.5);
+		const Color darkSquareColor = Color(0.40, 0.30, 0.5);
+		const Color borderColor = Color(0.40, 0.30, 0.5);
 
-	ChessBoard* board = new ChessBoard(
-		innerSize,
-		xBorderSize,
-		yBorderSize,
-		thickness,
-		lightSquareColor,
-		darkSquareColor,
-		borderColor);
+		// Create the chess board and add it to the scene.
 
-	board->Move(0.0, 0.0, -20.0);
+		ChessBoard* board = new ChessBoard(
+			innerSize,
+			xBorderSize,
+			yBorderSize,
+			thickness,
+			lightSquareColor,
+			darkSquareColor,
+			borderColor);
 
-	//board->RotateZ(+11.0);
-	//board->RotateX(-62.0);
+		board->Move(0.0, 0.0, -20.0);
 
-	scene.AddSolidObject(board);
+		//board->RotateZ(+11.0);
+		//board->RotateX(-62.0);
 
-	Cone* conoIzquierdo2 = new Cone(2.0, 4.0);
-	conoIzquierdo2->SetFullMatte(Color(0.6, 0.2, 0.2));
-	conoIzquierdo2->Move(-5.0, 4.0, -10.0);
-	conoIzquierdo2->RotateX(-90.0);
-	scene.AddSolidObject(conoIzquierdo2);
+		scene.AddSolidObject(board);
 
+		scene.AddLightSource(LightSource(Vector(-45.0, +25.0, +50.0), Color(0.4, 0.4, 0.1, 1.0)));
+		scene.AddLightSource(LightSource(Vector(+5.0, +90.0, +40.0), Color(0.5, 0.5, 1.5, 1.0)));
+		scene.AddLightSource(LightSource(Vector(-25.0, +30.0, +40.0), Color(0.3, 0.2, 0.1, 1.0)));
+		*/
 
-	/*printf("create background \n");
-	backGround* backGroundImage = new backGround(bg);
-	scene.AddSolidObject(backGroundImage);
+		//CUBOIDS
 
-	printf("create sphere \n");
-	const Vector sphereCenter(0.0, 0.0, -40.0);
-	const Vector lightDisplacement(-30.0, +120.0, +50.0);
+	Cuboid *cuboid = new Cuboid(0.5, 0.5, 0.9);
+	cuboid->SetFullMatte(Color(0.9, 0.5, 0.8));
+	cuboid->Move(1.2, -0.7, -50.0);
+	cuboid->RotateX(-130.0);
+	cuboid->RotateY(-3.0);
 
-	Sphere* sphere = new Sphere(sphereCenter, 5.5);
-	sphere->SetFullMatte(Color(0.6, 0.2, 0.2));
-	scene.AddSolidObject(sphere);
+	scene.AddSolidObject(cuboid);
+
+	Cuboid *cuboid2 = new Cuboid(0.3, 0.3, 0.9);
+	cuboid2->SetFullMatte(Color(0.9, 0.9, 0.8));
+	cuboid2->Move(1.9, -2.0, -50.0);
+	cuboid2->RotateX(-160.0);
+	cuboid2->RotateY(-3.0);
+
+	scene.AddSolidObject(cuboid2);
+
+	/*Cuboid *cuboid3 = new Cuboid(0.3, 0.3, 0.6);
+	cuboid2->SetFullMatte(Color(0.9, 0.9, 0.8));
+	cuboid2->Move(0.2, -0.9, -50.0);
+	cuboid2->RotateX(-160.0);
+	cuboid2->RotateY(-3.0);
+
+	scene.AddSolidObject(cuboid3);*/
+
+	/*Cuboid *cuboid4 = new Cuboid(0.4, 0.4, 0.6);
+	cuboid4->SetFullMatte(Color(0.9, 0.9, 0.8));
+	cuboid4->Move(2.1, -2.9, -50.0);
+	cuboid4->RotateX(-160.0);
+	cuboid4->RotateY(-3.0);
+
+	scene.AddSolidObject(cuboid4);
+	scene.AddLightSource(LightSource(Vector(-5.0, 50.0, +20.0), Color(1.0, 1.0, 1.0)));
 	*/
-	//Add a light source to illuminate the objects in the scene; otherwise we won't see anything!
-	scene.AddLightSource(LightSource(Vector(-45.0, +25.0, +50.0), Color(0.4, 0.4, 0.1, 1.0)));
 
-	scene.AddLightSource(LightSource(Vector(-100.0, 50.0, 100.0), Color(1.0, 1.0, 1.0, 1.0)));
-	scene.AddLightSource(LightSource(Vector(0.0, -20.0, 200.0), Color(1.0, 1.0, 1.0, 1.0)));//Vector(0.0, -20.0, 200.0)
-	scene.AddLightSource(LightSource(Vector(100.0, 50.0, 100.0), Color(1.0, 1.0, 1.0, 1.0)));//100.0, 50.0, 100.0)
 
-	printf("saving image \n");
-	const char *filename = "image.png";
-	scene.SaveImage(filename, 1008, 567, 3.0, 1);
+
+	//SPHERES
+
+	struct sphere_info
+	{
+		Vector center;
+		double radius;
+	};
+
+	const sphere_info sinfo[] =
+	{
+		//Arriba
+		{ Vector(-1.5, 0.1, -17.0), 0.32 },
+		{ Vector(-1.0, 0.6, -17.0), 0.20 },
+		{ Vector(-0.9, 0.2, -17.0), 0.18 },
+		{ Vector(-1.9, 0.6, -17.0), 0.12 },
+
+		//En medio
+		{ Vector(-1.0,  0.0, -17.0), 0.09 },
+		{ Vector(-1.2, -0.1, -17.0), 0.09 },
+		{ Vector(-0.8, -0.2, -17.0), 0.10 },
+		{ Vector(-1.2, -0.3, -17.0), 0.12 },
+		{ Vector(-0.1, -0.5, -17.0), 0.19 },
+		{ Vector(-1.7, -0.4, -17.0), 0.12 },
+		{ Vector(-1.4, -0.5, -17.0), 0.19 },
+		{ Vector(-1.8, -0.6, -17.0), 0.10 },
+
+		//abajo
+		{ Vector(-1.0, -0.6, -17.0), 0.08 },
+		{ Vector(-0.7, -0.9, -17.0), 0.08 },
+		{ Vector(-0.9, -0.7, -17.0), 0.07 },
+		{ Vector(-1.2, -1.0, -17.0), 0.06 },
+		{ Vector(-1.0, -1.2, -17.0), 0.11 },
+		{ Vector(-1.5, -1.0, -17.0), 0.06 },
+		{ Vector(-1.6, -1.2, -17.0), 0.11 }
+
+	};
+
+
+	const size_t numSpheres = sizeof(sinfo) / sizeof(sinfo[0]);
+	for (size_t i = 0; i < numSpheres; ++i)
+	{
+		Sphere* sphere = new Sphere(sinfo[i].center, sinfo[i].radius);
+		//sphere->Move(1.0, 0.0, 0.0);
+		//sphere->SetOpacity(0.17);
+		//sphere->SetMatteGlossBalance(0.95, Color(0.4, 0.5, 0.7), Color(0.8, 1.0, 0.7));
+		scene.AddSolidObject(sphere);
+	}
+
+	scene.AddLightSource(LightSource(Vector(-25.0, +30.0, +40.0), Color(0.3, 0.2, 0.1, 1.0)));
+	//CYLENDER
+
+	Cylinder* cylinder = new Cylinder(1.5, 4.0);
+	cylinder->SetFullMatte(Color(1.0, 0.5, 0.5));
+	cylinder->Move(-4.0, 2.0, -50.0);
+	cylinder->RotateX(-160.0);
+	cylinder->RotateY(10.0);
+
+	scene.AddSolidObject(cylinder);
+
+	Cylinder* cylinder2 = new Cylinder(1.5, 4.0);
+	cylinder2->SetFullMatte(Color(0.9, 0.2, 0.5));
+	cylinder2->Move(1.0, 2.5, -50.0);
+	cylinder2->RotateX(-120.0);
+	cylinder2->RotateY(5.0);
+
+	scene.AddSolidObject(cylinder2);
+
+
+	Cylinder* cylinder3 = new Cylinder(1.5, 4.0);
+	cylinder3->SetFullMatte(Color(0.1, 0.8, 0.6));
+	cylinder3->Move(6.0, 2.5, -50.0);
+	cylinder3->RotateX(-120.0);
+	cylinder3->RotateY(-180.0);
+
+	scene.AddSolidObject(cylinder3);
+	/*
+	Cylinder* cylinder4 = new Cylinder(0.5, 1.9);
+	cylinder4->SetFullMatte(Color(0.3, 0.8, 0.5));
+	cylinder4->Move(3.0, 4.0, -50.0);
+	cylinder4->RotateX(-190.0);
+	cylinder4->RotateY(-70.0);
+
+
+	scene.AddSolidObject(cylinder4);*/
+	scene.AddLightSource(LightSource(Vector(+35.0, +50.0, +20.0), Color(1.0, 1.0, 1.0)));
+
+
+	//Polyhedra
+
+	/*Optics optics;  // use default optics (white matte finish, opaque)
+
+	Dodecahedron* dodecahedron = new Dodecahedron(Vector(+0.0, -0.3, -50.0), 0.3, optics);
+	dodecahedron->RotateX(-12.0);
+	dodecahedron->RotateY(-7.0);
+
+	scene.AddSolidObject(dodecahedron);
+
+	Icosahedron* icosahedron = new Icosahedron(Vector(-2.0, -1.2, -50.0), 0.3, optics);
+	icosahedron->RotateY(-12.0);
+	icosahedron->RotateX(-7.0);
+
+	scene.AddSolidObject(icosahedron);
+	*/
+	scene.AddLightSource(LightSource(Vector(-45.0, +10.0, +50.0), Color(1.0, 1.0, 0.3, 1.0)));
+	scene.AddLightSource(LightSource(Vector(+5.0, +90.0, -40.0), Color(0.5, 0.5, 1.5, 0.5)));
+	scene.AddLightSource(LightSource(Vector(+45.0, -10.0, +40.0), Color(0.1, 0.5, 0.1, 0.5)));
+
+
+	const char* filename = "test.png";
+	scene.SaveImage(filename, 600, 360, 4.5, 3);
 	std::cout << "Wrote " << filename << std::endl;
 }
 
